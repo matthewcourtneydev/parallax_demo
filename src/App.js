6a1f5ef3099c13b0fb457f923b57dabea73dfb16
Matthew Courtney <mdcdev23@gmail.com>
2023-08-16 19:21:34 -0400

    mdc_parallax_demo1 got it working i am still playing around with the speeds if the discrepency is too much there is a space between pages fixing simply creates the issue with another screen size. may need to base speeds on scree size

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-
-import './App.css';
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import TextPage from './components/textPage';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-Hello World
+      <Parallax pages={2} style={{ top: "0", left: "0" }}  className="animation">
+        <ParallaxLayer offset={0} speed={0.25}>
+        <div className="animation_layer parallax" id="background"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.25}>
+        <div className="animation_layer parallax" id="mountain"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={-0.1}>
+        <div className="animation_layer parallax" id="logo_land"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+        <div className="animation_layer parallax" id="jungle1"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.45}>
+        <div className="animation_layer parallax" id="jungle2"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.35}>
+        <div className="animation_layer parallax" id="jungle3"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.25}>
+        <div className="animation_layer parallax" id="jungle4"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.20}>
+        <div className="animation_layer parallax" id="mountain_man"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.15}>
+        <div className="animation_layer parallax" id="jungle5"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.1}>
+        <TextPage />
+        </ParallaxLayer>
+        
+      </Parallax>
     </div>
   );
 }
---
+++ src/components/textPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const TextPage = () => {
+    return (
+        <div id="textblock">
+            <div id="textblock-container">
+                <h1 id="textblock-title">What is Firewatch?</h1>
+                <p id="textblock-content">
+                Lorem ipsum dolor sit amet. <br /><br />
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores reiciendis enim iste vitae repellendus! Nihil, officiis numquam iusto eaque inventore magnam. Explicabo unde tempore nam veritatis totam, molestias minus sint. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae quam officia nulla eum vel itaque omnis, nihil minus repellat. Repellendus quibusdam labore a voluptatum impedit nesciunt harum, reprehenderit voluptatibus!
+                </p>
+            </div>
+            
+        </div>
+    );
+}
+
+export default TextPage;
